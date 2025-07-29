722a8c526d8efb1111c2f21c293e5755b3fa6a7d
Harsh Kumar Rai <harshrai24685@gmail.com>
2025-07-30 00:36:45 +0530

    Learning

--- JS-Again/four.js
+++ JS-Again/four.js
@@ -2,7 +2,7 @@
 let a=[1,2,,3,4];
 //console.log(a[0]);
 let [e,b,c,d,f,g]=a;
-console.log(g);
+// console.log(g);
 
 let p={
 name:'harsh',
@@ -13,8 +13,10 @@ address:{
     city:"varanasi"
 }
 };
-let {name,age,gender,address:{country:cn}}=p;
-console.log(name,age,gender,c); //it is giving undefined because diffrent keys
-//you use only object keys to destructure object
-let {name:n,age:ag,gender:gn}=p;
-console.log(n,ag,gn);
+let {name:n,age,gender,address}=p;
+console.log(n);
+// let {name,age,gender,address:{country:cn}}=p;
+// console.log(name,age,gender,c); //it is giving undefined because diffrent keys
+// //you use only object keys to destructure object
+// let {name:n,age:ag,gender:gn}=p;
+// console.log(n,ag,gn);
